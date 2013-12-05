4a0794e951ba206d9701c3645ce434f6fbedfb1e
Mesalu <samuel@mesalu.com>
2013-12-04 22:00:32 -0800

    Added functionality to the help button.

--- src/com/mesalu/clarkmapp/MainActivity.java
+++ src/com/mesalu/clarkmapp/MainActivity.java
@@ -3,13 +3,25 @@ package com.mesalu.clarkmapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		View mapButton = findViewById(R.id.map_button);
+		mapButton.setOnClickListener(this);
+		View findButton = findViewById(R.id.find_button);
+		findButton.setOnClickListener(this);
+		View helpButton = findViewById(R.id.help_button);
+		helpButton.setOnClickListener(this);
+		
+		
 	}
 
 	@Override
@@ -19,4 +31,12 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void onClick(View v) {
+		switch (v.getId()){
+		case R.id.help_button:
+			Intent i = new Intent(this, HelpActivity.class);
+			startActivity(i);
+			break;
+		}
+	}
 }
