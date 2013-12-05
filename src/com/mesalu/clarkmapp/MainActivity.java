336b7502249e060d018ed1fa7eb01f340bf9cc12
Mesalu <samuel@mesalu.com>
2013-12-04 19:19:24 -0800

    adding rest of project in.

--- src/com/mesalu/clarkmapp/MainActivity.java
+++ src/com/mesalu/clarkmapp/MainActivity.java
@@ -3,22 +3,19 @@ package com.mesalu.clarkmapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.content.Intent;
 
-public class SplashActivity extends Activity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
-		Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        SplashActivity.this.startActivity(intent);
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
